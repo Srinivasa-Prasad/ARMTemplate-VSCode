b1f59f0b41f49ac35bb3bfd503dbe059de99be1d
Prasad, Srinivasa <srinivasa.prasad@anz.com>
2018-11-30 13:56:39 +0530

    File -> URI in ps1

--- DeployTemplate.ps1
+++ DeployTemplate.ps1
@@ -1,5 +1,5 @@
 ﻿Import-Module AzureRm
 
-## Connect-AzureRmAccount
+Connect-AzureRmAccount
 New-AzureRmResourceGroup -Name VSCode -Location eastus -Force
-New-AzureRmResourceGroupDeployment -Name VSCode -ResourceGroupName VSCode -TemplateFile "C:\NotBackedUp\SrinivasaLabs\AzureVSCode\DeployTemplate.json" -TemplateParameterFile "C:\NotBackedUp\SrinivasaLabs\AzureVSCode\DeployTemplate.parameters.json"
+New-AzureRmResourceGroupDeployment -Name VSCode -ResourceGroupName VSCode -TemplateFile https://raw.githubusercontent.com/Srinivasa-Prasad/ARMTemplate-VSCode/master/DeployTemplate.json -TemplateParameterUri https://raw.githubusercontent.com/Srinivasa-Prasad/ARMTemplate-VSCode/master/DeployTemplate.parameters.json
