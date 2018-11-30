962fd5a57cfe325a10099fcc4f1b5e48a959f49b
Prasad, Srinivasa <srinivasa.prasad@anz.com>
2018-11-30 13:50:02 +0530

    Initial Commit

---
+++ DeployTemplate.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module AzureRm
+
+## Connect-AzureRmAccount
+New-AzureRmResourceGroup -Name VSCode -Location eastus -Force
+New-AzureRmResourceGroupDeployment -Name VSCode -ResourceGroupName VSCode -TemplateFile "C:\NotBackedUp\SrinivasaLabs\AzureVSCode\DeployTemplate.json" -TemplateParameterFile "C:\NotBackedUp\SrinivasaLabs\AzureVSCode\DeployTemplate.parameters.json"
